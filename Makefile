liskin/strava-offline

commit 24385fc0dc6a5eac4b6e8811de693ac7d9b986df

Workaround failing checks on Debian/Ubuntu (setuptools 60+)
Debian patches setuptools/distutils, but not the distutils copy embedded
in setuptools, so https://github.com/pypa/pip/issues/6264 results in

    AttributeError: install_layout

Force SETUPTOOLS_USE_DISTUTILS=stdlib until (if ever) the issue is resolved.

Makefile
@@ -84,6 +84,11 @@ define VENV_CREATE_SYSTEM_SITE_PACKAGES
 	touch $(VENV_SYSTEM_SITE_PACKAGES)
 endef
 
+# workaround for https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1003252 and/or https://github.com/pypa/pip/issues/6264
+ifdef VENV_USE_SYSTEM_SITE_PACKAGES
+$(VENV_DONE): export SETUPTOOLS_USE_DISTUTILS := stdlib
+endif
+
 $(VENV_DONE): $(MAKEFILE_LIST) setup.py setup.cfg pyproject.toml
 	$(if $(VENV_USE_SYSTEM_SITE_PACKAGES),$(VENV_CREATE_SYSTEM_SITE_PACKAGES),$(VENV_CREATE))
 	$(VENV_PYTHON) -m pip install -e $(VENV_PIP_INSTALL)

commit aabd3f71c6be7ccebde7b181264ce37ac85a4010

Restrict SETUPTOOLS_USE_DISTUTILS=stdlib workaround
This was previously always used because "ifdef" in make doesn't expand
the variable. Fix the condition to expand the variable (this requires
that the venv-system-site-packages invokes make recursively) and also
add an additional check to only use this on Debian-based systems with
old Python.

Fixes: 24385fc0dc6a ("Workaround failing checks on Debian/Ubuntu (setuptools 60+)")

Makefile
@@ -8,8 +8,8 @@ VENV_SYSTEM_SITE_PACKAGES = $(VENV)/.venv-system-site-packages
 VENV_USE_SYSTEM_SITE_PACKAGES = $(wildcard $(VENV_SYSTEM_SITE_PACKAGES))
 
 .PHONY: venv-system-site-packages
-venv-system-site-packages: override VENV_USE_SYSTEM_SITE_PACKAGES := 1
-venv-system-site-packages: $(VENV_DONE)
+venv-system-site-packages:
+	$(MAKE) VENV_USE_SYSTEM_SITE_PACKAGES=1 venv
 
 .PHONY: venv
 venv: $(VENV_DONE)
@@ -95,8 +95,11 @@ define VENV_CREATE_SYSTEM_SITE_PACKAGES
 endef
 
 # workaround for https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1003252 and/or https://github.com/pypa/pip/issues/6264
-ifdef VENV_USE_SYSTEM_SITE_PACKAGES
+ifneq ($(VENV_USE_SYSTEM_SITE_PACKAGES),)
+ifneq ($(shell test -f /etc/debian_version && python3 -c 'import sys; exit(not(sys.version_info < (3, 10)))' && echo x),)
+$(info XXX: using SETUPTOOLS_USE_DISTUTILS=stdlib workaround)
 $(VENV_DONE): export SETUPTOOLS_USE_DISTUTILS := stdlib
+endif
 endif
 
 $(VENV_DONE): $(MAKEFILE_LIST) setup.py setup.cfg pyproject.toml