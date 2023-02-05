46c971775cde5e266fd105730a415dc8400f04ab
Tomas Janousek <tomi@nomi.cz>
2023-02-05 00:26:08 +0000

    Avoid hardcoding script entry point in __main__

--- src/strava_offline/__init__.py
+++ src/strava_offline/__init__.py
@@ -1,7 +1,11 @@
-try:
-    import importlib.metadata as importlib_metadata  # type: ignore # isort: skip
-except ImportError:
-    import importlib_metadata  # type: ignore # isort: skip
+# type: ignore
+
+import sys
+
+if sys.version_info >= (3, 10):
+    import importlib.metadata as importlib_metadata  # isort: skip
+else:
+    import importlib_metadata  # isort: skip
 
 try:
     __version__ = importlib_metadata.version(__name__)
